641921f0798c3758990c7a1457b1cd58f9274a7b
Socool <hi.seokhowon@gmail.com>
2016-07-19 03:21:41 +0900

    Sending an email new

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -3,7 +3,19 @@ class EmailController < ApplicationController
   end
 
   def email_send
-    @email = params[:email]
-    @content = params[:content]
-  end
+    email = params[:email]
+    title = params[:title]
+    content = params[:content]
+   
+    gmail = Gmail.connect("hi.sokhowon@gmail.com","")
+    gmail.deliver do
+      to email
+      subject title
+      text_part do
+        body content
+      end
+    end
+
+    gmail.logout
+  end 
 end
